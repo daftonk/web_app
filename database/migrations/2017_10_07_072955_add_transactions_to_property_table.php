b6f1050abbb143ae06881c459136b78020992aa2
ckchivatsi <kelvinchivatsi@hotmail.com>
2017-10-07 10:57:55 +0300

    added transactions to property table migration

---
+++ database/migrations/2017_10_07_072955_add_transactions_to_property_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTransactionsToPropertyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('property', function (Blueprint $table) {
+            // $table->increments('id');
+            // $table->string('name');
+            $table->string('transaction');
+            $table->integer('category_id')->unsigned()->change();
+            $table->foreign('category_id')->references('id')->on('categories');
+            // $table->string('location');
+            // $table->decimal('latitude', 10, 7);
+            // $table->decimal('longitude', 10, 7);
+            // $table->integer('price');
+            // $table->string('status');
+            // $table->longText('details');
+            // $table->string('images');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('property', function (Blueprint $table) {
+            //
+        });
+    }
+}
